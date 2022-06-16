4afe910360fc66018d050d1934861b980b128fe7
Andra Ramadhan <andraram18@gmail.com>
2022-06-16 13:21:08 +0700

    fix assets

--- main/app/Views/home/index.php
+++ main/app/Views/home/index.php
@@ -9,19 +9,19 @@
     <meta content="Best Scraper For CPA" name="description">
 
     <!-- Favicons -->
-    <link href="/assets/images/favicon.ico" rel="icon">
-    <link href="/assets/images/favicon.ico" rel="apple-touch-icon">
+    <link href="assets/images/favicon.ico" rel="icon">
+    <link href="assets/images/favicon.ico" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="/assets/home/vendor/aos/aos.css" rel="stylesheet">
-    <link href="/assets/home/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/assets/home/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="/assets/home/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="/assets/home/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="/assets/home/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/home/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/home/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/home/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/home/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/home/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/home/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
     <link href="assets/home/css/style.css" rel="stylesheet">
@@ -190,13 +190,13 @@
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <!-- <script src="/assets/home/vendor/purecounter/purecounter.js"></script> -->
-    <script src="/assets/home/vendor/aos/aos.js"></script>
-    <script src="/assets/home/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/home/vendor/glightbox/js/glightbox.min.js"></script>
-    <script src="/assets/home/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-    <script src="/assets/home/vendor/swiper/swiper-bundle.min.js"></script>
-    <script src="/assets/home/vendor/php-email-form/validate.js"></script>
+    <!-- <script src="assets/home/vendor/purecounter/purecounter.js"></script> -->
+    <script src="assets/home/vendor/aos/aos.js"></script>
+    <script src="assets/home/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/home/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/home/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/home/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/home/vendor/php-email-form/validate.js"></script>
 
     <!-- Template Main JS File -->
     <script src="assets/home/js/main.js?v=2"></script>
